06536f17fdea6e5279d6b34adb8ccec407cea302
Angelica <santosangelicassp@gmail.com>
2020-09-23 16:11:11 -0300

    att2 colecao

--- src/com/digitalhouse/br/Aula12/ExercicioIntegrador/Colecao.kt
+++ src/com/digitalhouse/br/Aula12/ExercicioIntegrador/Colecao.kt
@@ -2,4 +2,5 @@ package com.digitalhouse.br.Aula12.ExercicioIntegrador
 
 class Colecao (val codigo: Int, val preco: Double, val descricao: String, val listaLivros: MutableList<Livro>) {
 
+    //atualizacao colecao
 }
